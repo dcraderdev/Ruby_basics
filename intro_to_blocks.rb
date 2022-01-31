eabd7b266a2881425644a2d19953eab914e4fadb
donovancrader <dcraderdev@gmail.com>
2022-01-30 19:43:52 -0800

    Finished intro to blocks
    We're learnin'!

--- intro_to_blocks.rb
+++ intro_to_blocks.rb
@@ -35,6 +35,11 @@ puts statements
 
 
 
+
+
+
+
+
 # # the yield keyword
 
 # the yield keyword transfers control from the method to the block that
@@ -77,6 +82,12 @@ multiple_pass { puts "Now I'm inside the block" }
 # Now I'm inside the block
 # Back inside the method
 # Now I'm inside the block
+
+
+
+
+
+
 
 
 
@@ -171,6 +182,10 @@ p ages.reject(&is_old)
 
 
 
+
+
+
+
 # # the .block given boolean/predicate method
 
 #enables some security in case a block is not attached to a method call that expects one
@@ -191,6 +206,11 @@ pass_control_on_condition { puts "Hello there" }
 pass_control_on_condition       # no block given so we just move through the method
 # Inside the method
 # Back inside the method
+
+
+
+
+
 
 
 
@@ -228,6 +248,8 @@ p result
 # 750
 # here we are calling for a method within the block in our 
 # argument that will replace the yield keyword
+
+
 
 
 
@@ -275,6 +297,8 @@ custom_each(numbers) { |number| puts "The square of #{number} is #{number ** 2}"
 
 
 
+
+
 # # PROCS II
 
 def greeter
@@ -299,15 +323,66 @@ hi.call
 
 
 
-# # Pass a Ruby method as a proc
+
+
+
+# # Pass a Ruby method as a proc with the &:
 
 
 
 p ["1", "2", "3"].map { |number| number.to_i }
+# [1, 2, 3]
 # this is a longer approach then we need
 
 p ["1", "2", "3"].map(&:to_i)
-# we can use this syntax with the to_index method used as a symbol with the :
+# [1, 2, 3]
+
+# we can use this syntax with the to_index method used as a symbol with the &:
+# we need to convert the variable inside the () to a symbol so that its recognized as 
+# as a string like object to Ruby for this method
+# so we use :
+# and we need to pass the Proc to the method
+# so we use &
+
+p [10, 14, 25].map(&:to_s)
+["10", "14", "25"]
+# converts to a string
+
+p [1, 2, 3, 4, 5].select(&:even?)
+p [1, 2, 3, 4, 5].reject(&:odd?)
+
+
+
+
+
+
+
+
+# # methods with Proc parameters
+
+# a mehtod can anticipate a proc as a parameter...
+
+def talk_about(name, &myprc)
+# if we want to use a proc argument we have to use & inside our method parameters
+  puts "Lets me tell you about #{name}."
+  myprc.call(name)
+end
+# have to use the .call method on a proc 
+# does the same thing as yielding but used on a proc that has multple possible procs  
+
+good_things = Proc.new do |name|
+  puts "#{name} is a genius!"
+end
+
+
+bad_things = Proc.new do |name|
+  puts "#{name} is a fool!"
+end
+
+talk_about("Donovan", &good_things)
+# still need to include the & proc call symobl that coresponds with the method arguments
+
+talk_about("Mona", &bad_things)
 
 
 
@@ -317,8 +392,169 @@ p ["1", "2", "3"].map(&:to_i)
 
 
 
+# # intro to lambdas
 
 
+# nearly identical to procs but lamdas tend to break or return an error
+# when too many parameters/wrong number of arguments are entered
+# compared to Procs where it would not break and return nil for any extra parameters
+
+squares_lambda = lambda { |number| number ** 2 }
+# with lambda we don't need the .new
+squares_proc = Proc.new { |number| number ** 2 }
+
+p [1, 2, 3].map(&squares_proc)
+# [1, 4, 9]
+# here we are mapping over the array and calling the proc on each new array
+p squares_proc.call(5)
+# 25
+# here we use the .call method on our proc and then tell Ruby we want to use it on (5)
+
+
+p [1, 2, 3].map(&squares_lambda)
+# [1, 4, 9]
+p squares_lambda.call(5)
+# 25
+# theyre the same as above! so far...
+
+
+
+some_proc = Proc.new { |name, age| "Your name is #{name} and your age is #{age}" }
+
+p some_proc.call("Jim")
+# "Your name is Jim and your age is "
+p some_proc.call("Jim", 25)
+# "Your name is Jim and your age is 25"
+
+# since we used a proc, the code still returned somewhat usable code 
+# because the proc doesnt care about the # of arguments
+
+
+some_lambda = lambda { |name, age| "Your name is #{name} and your age is #{age}" }
+
+# some_lambda.call("Jim")
+# returns an error --
+# `block in <main>': wrong number of arguments (given 1, expected 2) (ArgumentError)
+
+p some_lambda.call("Jim", 25)
+# "Your name is Jim and your age is 25"
+
+
+def diet
+  status = lambda { return "You gave in" }
+  status.call
+  "You completed the diet!"
+end
+
+result = diet
+p result
+"You completed the diet!"
+# when return is used within a block for a lambda...
+# the return will return control back to the method
+
+
+def diet
+  status = Proc.new { return "You gave in" }
+  status.call
+  "You completed the diet!"
+end
+
+result = diet
+p result
+"You gave in"
+# when return is used within a block for a Proc...
+# the return "value" or whatever it is set to do will break out and be returned
+# this could break the flow of the program
+
+
+
+
+
+
+
+
+
+# # Lambda efficiency example
+
+# we can use lambdas to make our code more efficient and more adaptable
+# also used to refactor to make code shorter and simpler
+
+def convert_to_euros(dollars)
+  if dollars.is_a?(Numeric)
+    dollars * 0.95
+  end
+end
+
+def convert_to_pesos(dollars)
+  if dollars.is_a?(Numeric)
+    dollars * 20.67
+  end
+end
+
+def convert_to_rupees(dollars)
+  if dollars.is_a?(Numeric)
+    dollars * 68.13
+  end
+end
+
+# stage 1 of writing some currecny conversion program
+
+
+# refactored a little... 
+
+def convert_to_euros(dollars)
+  dollars * 0.95 if dollars.is_a?(Numeric)
+end
+
+def convert_to_pesos(dollars)
+ dollars * 20.67 if dollars.is_a?(Numeric)
+end
+
+def convert_to_rupees(dollars)
+  dollars * 68.13 if dollars.is_a?(Numeric)
+end
+
+
+# refactored with a yiled...
+
+def convert(dollars, currency)
+  yield(dollars, currency) if dollars.is_a?(Numeric)
+end
+
+# we've copied the repeated method into the yield method, which will make the method iterate 
+# over each part of the array with the custom block
+
+p convert(1000, "euros") { |dollars| dollars * 0.95 }
+p convert(1000, "pesos") { |dollars| dollars * 20.67 }
+p convert(1000, "rupess") { |dollars| dollars * 68.13 }
+
+# but you can see when we still need to write a lot of reapeating information for each
+# and its not easy to edit if anything needed to be changed
+
+
+# refactored with a lambda
+
+to_euros = lambda { |dollars| dollars * 0.95 }
+to_pesos = lambda { |dollars| dollars * 20.67 }
+to_rupees = lambda { |dollars| dollars * 68.13 }
+# the variables inside the {} need to match
+# but are not the same as the variables inside our parameters/argument
+# here we've set block methods to take effect when called on when we perform a method
+# and we can edit the code in only one part instead of each custom block
+
+
+def convert(dollars2, currency_lambda)
+  currency_lambda.call(dollars2) if dollars2.is_a?(Numeric)
+end
+
+p convert(1000, to_euros) 
+p convert(1000, to_pesos) 
+p convert(1000, to_rupees)
+# 950.0
+# 20670.0
+# 68130.0
+
+# in total we've shrunk 21 lines of code to 9 and it is much more efficient
 
 
 
